6b13c3ae7c335d4082ca685caef540d8cee1b05c
Luy_0 <seven_ou@163.com>
2020-10-22 11:15:44 +0800

    Add:bulletinBar

---
+++ components/bulletinBar/bulletinBar.js
@@ -0,0 +1,35 @@
+// components/bulletinBar/bulletinBar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    msgList:[
+      {
+        title:"title1"
+      },
+      {
+        title:"title2"
+      },
+      {
+        title:"title3"
+      }
+    ],
+    interval: 2000,
+    duration: 500
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
